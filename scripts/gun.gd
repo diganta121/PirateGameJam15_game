c8cae68cfc76c4cc830cdd596c68935b6e88f1ec
dig <duttadiganta049@gmail.com>
2024-07-19 01:59:47 +0530

    fisgh with guns

--- scripts/gun.gd
+++ scripts/gun.gd
@@ -1,10 +1,13 @@
 extends Area2D
-
+var enable_shoot = false
 func _physics_process(delta):
 	var enemies = get_overlapping_bodies()
 	if enemies.size() > 0 :
+		enable_shoot = true
 		var targer_enemy = enemies.front()
 		look_at(targer_enemy.global_position)
+	else :
+		enable_shoot = false
 		
 func shoot():
 	const BULLET = preload("res://scenes/bullet.tscn")
@@ -14,4 +17,5 @@ func shoot():
 	%ShootingPoint.add_child(new_bullet)
 
 func _on_timer_timeout():
-	shoot()
+	if enable_shoot:
+		shoot()
--- scripts/player.gd
+++ scripts/player.gd
@@ -9,16 +9,15 @@ func _physics_process(delta):
 	velocity = direc * 650
 	move_and_slide()
 	
-	if velocity.length() > 0.0:
-		get_node("CollisionShape2D/HappyBoo").play_walk_animation()
-	else :
-		get_node("CollisionShape2D/HappyBoo").play_idle_animation()
+	#if velocity.length() > 0.0:
+		#get_node("CollisionShape2D/HappyBoo").play_walk_animation()
+	#else :
+		#get_node("CollisionShape2D/HappyBoo").play_idle_animation()
 	
 	const DAMAGE = 5.0
 	var over_mobs = %HurtBox.get_overlapping_bodies()
 	if over_mobs.size() >0 :
 		health -= DAMAGE * delta
-		%ProgressBar.value = health
 		if health <= 0.0:
 			player_dead.emit()
-			
+
