d6ca99a54562ad4d3fa9903538a162ca82b4dddd
dig <70334760+diganta121@users.noreply.github.com>
2024-07-25 15:27:48 +0530

    e

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,20 +3,31 @@ var health := 100.0
 var MOB_DAMAGE := 5.0
 signal player_dead
 var speedVAL := 650
-@onready var bass = $Bass
+const default_speed := 650
+@onready var Sprite = $playerSprite
 @onready var timer = $Timer
-
+var animationState := 1 
+const ANIMATION_STATE = {
+	1:'walk',
+	2:'strength',
+	3:'speed'
+}
 func _physics_process(delta) -> void:
 	var direc = Input.get_vector("move_left","move_right","move_up","move_down")
 
 	velocity = direc * speedVAL 
 	move_and_slide()
-	#if velocity.length() > 0.0:
-		#get_node("CollisionShape2D/HappyBoo").play_walk_animation()
-	#else :
-		#get_node("CollisionShape2D/HappyBoo").play_idle_animation()
+	if velocity.length() > 0.0:
+		Sprite.play("walk")
+		if direc.x < 0:
+			Sprite.flip_h = true
+		else:
+			Sprite.flip_h = false
+	else :
+		Sprite.play("idle")
+		
 	var over_mobs = %HurtBox.get_overlapping_bodies()
-	if over_mobs.size() >0 :
+	if over_mobs.size() > 0 :
 		health -= MOB_DAMAGE * delta
 		if health <= 0.0:
 			player_dead.emit()
@@ -26,15 +37,15 @@ func player_id() -> int:
 
 func aniplay():
 	timer.start()
-	bass.play("strength")
-	MOB_DAMAGE = 15.0	
+	Sprite.play("strength")
+	MOB_DAMAGE = 15.0
 
 func speedplay():
 	timer.start()
-	bass.play("speed")
-	speedVAL =1800
+	Sprite.play("speed")
+	speedVAL = 1800
 
 func _on_timer_timeout():
-	bass.play("idle")
+	Sprite.play("idle")
 	MOB_DAMAGE =5.0
 	speedVAL =650
