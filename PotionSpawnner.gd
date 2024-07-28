4d620ea1fbcc7479ca08a3450e20819c86c082e1
dig <70334760+diganta121@users.noreply.github.com>
2024-07-28 20:15:07 +0530

    potion spawner 0.5

---
+++ PotionSpawnner.gd
@@ -0,0 +1,18 @@
+extends Area2D
+@export var item_type = ""
+
+const ITEMS = {
+	"red":"",
+	"blue":"",
+	"gold":"",
+	"silver":""
+}
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
